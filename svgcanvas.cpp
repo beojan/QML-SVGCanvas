725d13940640abc9cbb60f1427b665c82018e86c
Beojan Stanislaus <beojan@gmail.com>
2015-04-14 12:34:44 +0100

    Fix aspect ratio issue

--- svgcanvas.cpp
+++ svgcanvas.cpp
@@ -5,6 +5,7 @@ SVGCanvas::SVGCanvas(QQuickItem *parent):
     QQuickPaintedItem(parent)
 {
     this->m_renderer = new QSvgRenderer(m_content, this);
+    connect(m_renderer, "repaintNeeded", this, "repaint");
 }
 
 QString SVGCanvas::content() const
@@ -20,10 +21,47 @@ void SVGCanvas::setContent(const QString& content)
     emit contentChanged();
 }
 
+bool SVGCanvas::animated() const
+{
+    return m_renderer->animated();
+}
+
+int SVGCanvas::fps() const
+{
+    return m_renderer->framesPerSecond();
+}
+
+void SVGCanvas::setFps(int fps)
+{
+    m_renderer->setFramesPerSecond(fps);
+}
+
+void SVGCanvas::repaint()
+{
+    this->update();
+}
+
 void SVGCanvas::paint(QPainter *painter)
 {
     painter->setRenderHints(QPainter::Antialiasing, true);
-    m_renderer->render(painter);
+    //Fix aspect ratio issue
+    QSize defSize = m_renderer->defaultSize();
+    QSizeF mySize = this->boundingRect().size();
+    qreal widthRatio = mySize.width() / defSize.width();
+    qreal heightRatio = mySize.height() / defSize.height();
+    if(widthRatio > heightRatio) {
+        mySize.rwidth() = defSize.width() * heightRatio;
+    }
+    else if(widthRatio < heightRatio) {
+        mySize.rheight() = defSize.height() * widthRatio;
+    }
+    QPointF newTopLeft = this->boundingRect().topLeft();
+    newTopLeft.rx() += (this->boundingRect().size().width() - mySize.width()) / 2;
+    newTopLeft.ry() += (this->boundingRect().size().height() - mySize.height()) / 2;
+    QRectF boundingBox(newTopLeft, mySize);
+    this->setImplicitWidth(mySize.width());
+    this->setImplicitHeight(mySize.height());
+    m_renderer->render(painter, boundingBox);
 }
 
 SVGCanvas::~SVGCanvas()
--- svgcanvas.h
+++ svgcanvas.h
@@ -9,6 +9,8 @@ class SVGCanvas : public QQuickPaintedItem
     Q_OBJECT
     Q_DISABLE_COPY(SVGCanvas)
     Q_PROPERTY(QString content READ content WRITE setContent NOTIFY contentChanged)
+    Q_PROPERTY(bool animated READ animated)
+    Q_PROPERTY(int fps READ fps WRITE setFps)
 public:
     SVGCanvas(QQuickItem *parent = 0);
     ~SVGCanvas();
@@ -16,9 +18,18 @@ public:
     QString content() const;
     void setContent(const QString& content);
 
+    bool animated() const;
+
+    int fps() const;
+    void setFps(int fps);
+
     void paint(QPainter *painter);
+public slots:
+    void repaint();
+
 signals:
     void contentChanged();
+
 
 private:
     QByteArray m_content;
